4eaf60043a6d88f81b4ca92ac2741cd50d909c37
georgepapagapitos <63572045+georgepapagapitos@users.noreply.github.com>
2025-03-04 20:49:10 -0600

    update configs (#5)

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,12 @@ module.exports = {
   rootDir: "src",
   testRegex: ".*\\.spec\\.ts$",
   transform: {
-    "^.+\\.(t|j)s$": "ts-jest",
+    "^.+\\.(t|j)s$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.test.json",
+      },
+    ],
   },
   collectCoverageFrom: ["**/*.(t|j)s"],
   coverageDirectory: "../coverage",
